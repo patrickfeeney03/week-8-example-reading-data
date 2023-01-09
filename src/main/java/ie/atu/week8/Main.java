9c14b1ee27cbaec579b05c95c281dcb5d2182621
Patrick <g00409592@atu.ie>
2023-01-09 13:44:23 +0000

    Empty commit

---
+++ src/main/java/ie/atu/week8/Main.java
@@ -0,0 +1,11 @@
+package ie.atu.week8;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
